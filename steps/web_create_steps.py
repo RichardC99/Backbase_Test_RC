87e7956f25dd39eb112fab38710d36029f27cebb
RichardC99 <30872932+RichardC99@users.noreply.github.com>
2019-05-18 23:52:50 +0100

    tidying up delete feature

--- steps/web_create_steps.py
+++ steps/web_create_steps.py
@@ -3,6 +3,7 @@ from modules.pages.create_and_edit_page import CreateAndEditPage
 from modules.pages.homepage import Homepage
 from modules.helpers import AbstractJanitor
 
+
 @given('computer <computer_name> does not exist')
 def step_impl(context, computer_name):
     page = Homepage(context.browser)
@@ -13,14 +14,8 @@ def step_impl(context, computer_name):
         if not (page.confirm_computer_present(computer_name)):
             break
 
-    context.execute_steps('Given A user has navigated to the BB_Test_Webpage')
     context.execute_steps('Given add a new computer is clicked')
-    context.execute_steps('Given the User enters BB_computer_for_edit_test computer name')
-    context.execute_steps('Given the user enters 1986-04-04 introduced date')
-    context.execute_steps('Given the user enters 2000-07-07 discontinued date')
-    context.execute_steps('Given the user selects Apple Inc. company')
-    context.execute_steps('When the user clicks Save_this_computer')
-    context.execute_steps('Given the User navigates to the Update Computer screen')
+
 
 
 @given('the User enters {computer_name} computer name')
