9981e92658716b1700c8128543b93fff04feec79
RichardC99 <30872932+RichardC99@users.noreply.github.com>
2019-05-20 11:49:16 +0100

    API Create tests added

--- modules/api/api.py
+++ modules/api/api.py
@@ -10,49 +10,86 @@ class API():
     def __init__(self):
         self.base_url = config.url
 
-    def create_computer(self, name, intro_date, discon_date, company ):
+    def create_computer(self, name, intro_date, discon_date, company):
+        if name == "null":
+            name = ""
 
-company = {
-            " Apple Inc." : 1,
-            " Thinking Machines" : 2,
-            " RCA" : 3,
-            " Netronics" : 4,
-            " Tandy Corporation" : 5,
-            " Commodore International" : 6,
-            " MOS Technology" : 7,
-            " Micro Instrumentation and Telemetry Systems" : 8,
-            " IMS Associates, Inc." : 9,
-            " Digital Equipment Corporation" : 10,
-            " Lincoln Laboratory" : 11,
-            " Moore School of Electrical Engineering" : 12,
-            " IBM" : 13,
-            " Amiga Corporation" : 14,
-            " Canon" : 15,
-            " Nokia" : 16,
-            " Sony" : 17,
-            " OQO" : 18,
-            " NeXT" : 19,
-            " Atari" : 20,
-            " Acorn computer" : 22,
-            " Timex Sinclair" : 23,
-            " Nintendo" : 24,
-            " Sinclair Research Ltd" : 25,
-            " Xerox" : 26,
-            " Hewlett-Packard" : 27,
-            " Zemmix" : 28,
-            " ACVS" : 29,
-            " Sanyo" : 30,
-            " Cray" : 31,
-            " Evans &amp; Sutherland" : 32,
-            " E.S.R. Inc." : 33,
-            " OMRON" : 34,
-            " BBN Technologies" : 35,
-            " Lenovo Group" : 36,
-            " ASUS" : 37,
-            " Amstrad" : 38,
-            " Sun Microsystems" : 39,
-            " Texas Instruments" : 40,
-            " HTC Corporation" : 41,
-            " Research In Motion" : 42,
-            " Samsung Electronics" : 43
-}
+        if intro_date == "null":
+            intro_date = ""
+
+        if discon_date == "null":
+            discon_date = ""
+
+        if company == "null":
+            company = ""
+        else:
+            company = self.get_company_code(company)
+
+        payload = {
+            "name": f"{name}",
+            "introduced": f"{intro_date}",
+            "discontinued": f"{discon_date}",
+            "company": f"{company}"
+        }
+
+        self.response = requests.post(self.base_url,json=payload)
+
+
+    def confirm_response_code(self, expected_code):
+        if str(self.response) == f"<Response [{expected_code}]>":
+            return True
+        return False
+
+
+
+
+
+
+    def get_company_code(self, company):
+
+
+        company_dict = {
+                    "Apple Inc.": 1,
+                    "Thinking Machines": 2,
+                    "RCA": 3,
+                    "Netronics": 4,
+                    "Tandy Corporation": 5,
+                    "Commodore International": 6,
+                    "MOS Technology": 7,
+                    "Micro Instrumentation and Telemetry Systems": 8,
+                    "IMS Associates, Inc.": 9,
+                    "Digital Equipment Corporation": 10,
+                    "Lincoln Laboratory": 11,
+                    "Moore School of Electrical Engineering": 12,
+                    "IBM": 13,
+                    "Amiga Corporation": 14,
+                    "Canon": 15,
+                    "Nokia": 16,
+                    "Sony": 17,
+                    "OQO": 18,
+                    "NeXT": 19,
+                    "Atari": 20,
+                    "Acorn computer": 22,
+                    "Timex Sinclair": 23,
+                    "Nintendo": 24,
+                    "Sinclair Research Ltd": 25,
+                    "Xerox": 26,
+                    "Hewlett-Packard": 27,
+                    "Zemmix": 28,
+                    "ACVS": 29,
+                    "Sanyo": 30,
+                    "Cray": 31,
+                    "Evans &amp; Sutherland": 32,
+                    "E.S.R. Inc.": 33,
+                    "OMRON": 34,
+                    "BBN Technologies": 35,
+                    "Lenovo Group": 36,
+                    "ASUS": 37,
+                    "Amstrad": 38,
+                    "Sun Microsystems": 39,
+                    "Texas Instruments": 40,
+                    "HTC Corporation": 41,
+                    "Research In Motion": 42,
+                    "Samsung Electronics": 43
+        }
+        return company_dict[company]
--- steps/api_create_steps.py
+++ steps/api_create_steps.py
@@ -2,6 +2,16 @@ from behave import given, when, then
 from modules.api.api import API
 
 
-@given('a user creates a new computer named {Computer_name} via the API')
-def step_impl(context, computer_name):
-    context.api =API()
+@given('a user creates a new record with name "{Computer_name}", intro_date "{Introduced_Date}", discon_date "{Discontinued_Date}" and company "{Company}"')
+def step_impl(context, Computer_name, Introduced_Date, Discontinued_Date, Company):
+    context.computer_name = Computer_name
+    context.intro_date = Introduced_Date
+    context.discon_date = Discontinued_Date
+    context.company = Company
+    context.api = API()
+
+    context.api.create_computer(Computer_name, Introduced_Date, Discontinued_Date, Company)
+
+@then('the response status should be {response_code}')
+def step_impl(context, response_code):
+    assert context.api.confirm_response_code(response_code)
